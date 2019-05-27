111346206ddf6622e63e64bd54f380d05bed5d34
Rasmus Klausen <166102@rts-365.dk>
2019-05-27 10:32:55 +0200

    commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,17 @@
 module.exports = (app) => {
+
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home',{
+         "titel":"min fancy forside"
+      });
    });
+
+   app.get('/product', (req, res, next) => {
+      res.render('product');
+   });
+
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
+
 };
